705319f794d8584afce3074c59d3e2e6ddacebaf
Victor Zuluaga <zrvictor@outlook.com>
2021-07-04 20:34:11 -0500

    measuring time of execution of my code

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,17 @@
+function countPrimeNumbers() {
+    let primeNumbers = [];
+    for (let i = 2; i <= 100; i++) {
+        for (let j = 1; j < i; j++) {
+            if (i % j == 0 && j != 1) {
+                break
+            } else if (j === i - 1) 
+                primeNumbers.push(i);
+        }
+    }
+    return primeNumbers;
+}
+
+t0 = performance.now();
+countPrimeNumbers();
+t1 = performance.now();
+console.log(`Execution time of printing countPrimeNumbers was ${t1 - t0} milliseconds.`);
